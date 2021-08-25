ab717b5d7606b17a813354840c7fccce7a248211
mfarhanSystemsltd <88446631+mfarhanSystemsltd@users.noreply.github.com>
2021-08-25 16:11:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Osman bhai!!\n');
+  res.send('Hello Arslaan bhai!!\n');
 });
 
 var port = process.env.PORT||PORT;
